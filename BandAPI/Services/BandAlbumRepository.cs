224264c64012645653704a7b91f05d0895cbe53c
Arun Thomas <arunthomas04@gmail.com>
2020-12-18 06:49:20 +0530

    updated

--- BandAPI/Services/BandAlbumRepository.cs
+++ BandAPI/Services/BandAlbumRepository.cs
@@ -13,67 +13,105 @@ namespace BandAPI.Services
 
         public BandAlbumRepository(BandAlbumContext context)
         {
-            this._context = context;
+            this._context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
         public void AddAlbum(Guid bandId, Album album)
         {
-            throw new NotImplementedException();
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+            if(album == null)
+                throw new ArgumentNullException(nameof(album));
+
+            album.BandId = bandId;
+            _context.Albums.Add(album);
         }
 
         public void AddBand(Band band)
         {
-            throw new NotImplementedException();
+            if (band == null)
+                throw new ArgumentNullException(nameof(band));
+
+            _context.Bands.Add(band);
         }
 
         public bool AlbumExists(Guid albumId)
         {
-            throw new NotImplementedException();
+            if (albumId == Guid.Empty)
+                throw new ArgumentNullException(nameof(albumId));
+
+            return _context.Albums.Any(a => a.Id == albumId);
         }
 
         public bool BandExists(Guid bandId)
         {
-            throw new NotImplementedException();
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            return _context.Bands.Any(b => b.Id == bandId);
         }
 
         public void DeleteAlbum(Album album)
         {
-            throw new NotImplementedException();
+            if (album == null)
+                throw new ArgumentNullException(nameof(album));
+
+            if (AlbumExists(album.Id))
+                _context.Albums.Remove(album);
         }
 
         public void DeleteBand(Band band)
         {
-            throw new NotImplementedException();
+            if (band == null)
+                throw new ArgumentNullException(nameof(band));
+
+            if (BandExists(band.Id))
+                _context.Bands.Remove(band);
         }
 
         public Album GetAlbum(Guid bandId, Guid albumId)
         {
-            throw new NotImplementedException();
+            if (albumId == Guid.Empty)
+                throw new ArgumentNullException(nameof(albumId));
+
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            return _context.Albums.FirstOrDefault(a => a.Id == albumId && a.BandId == bandId);
         }
 
         public IEnumerable<Album> GetAlbums(Guid bandId)
         {
-            throw new NotImplementedException();
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            return _context.Albums.Where(a => a.BandId == bandId).OrderBy(a => a.Title);
         }
 
         public Band GetBand(Guid bandId)
         {
-            throw new NotImplementedException();
+            if (bandId == Guid.Empty)
+                throw new ArgumentNullException(nameof(bandId));
+
+            return _context.Bands.FirstOrDefault(b => b.Id == bandId);
         }
 
         public IEnumerable<Band> GetBands()
         {
-            throw new NotImplementedException();
+            return _context.Bands.ToList();
         }
 
         public IEnumerable<Band> GetBands(IEnumerable<Guid> bandIds)
         {
-            throw new NotImplementedException();
+            if (bandIds == null)
+                throw new ArgumentNullException(nameof(bandIds));
+
+            return _context.Bands.Where(b => bandIds.Contains(b.Id)).OrderBy(b => b.Name).ToList();
         }
 
         public bool Save()
         {
-            throw new NotImplementedException();
+            return (_context.SaveChanges() >= 0);
         }
 
         public void UpdateAlbum(Album album)
