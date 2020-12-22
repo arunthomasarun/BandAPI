10a9e384be2c844a05cc60dc7d2b9a0726ee3741
Arun Thomas <ArunThomas04@gmail.com>
2020-12-22 06:57:21 +0530

    updated

---
+++ BandAPI/Controllers/BandsController.cs
@@ -0,0 +1,35 @@
+﻿using BandAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BandAPI.Controllers
+{
+    [ApiController]
+    [Route("/api/bands")]
+    public class BandsController : ControllerBase
+    {
+        private readonly IBandAlbumRepository _bandAlbumRepository;
+
+        public BandsController(IBandAlbumRepository bandAlbumRepository)
+        {
+            this._bandAlbumRepository = bandAlbumRepository ?? throw new ArgumentException(nameof(bandAlbumRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetBands()
+        {
+            var bands = _bandAlbumRepository.GetBands();
+            return Ok(bands);
+        }
+
+        [HttpGet("{bandId}")]
+        public IActionResult GetBand(Guid bandId)
+        {
+            var band = _bandAlbumRepository.GetBand(bandId);
+            return Ok(band);
+        }
+    }
+}
