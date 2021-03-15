31313fd588f6e5c07b5724e4b334436248939b77
Arun Thomas <ArunThomas04@gmail.com>
2021-03-15 07:00:29 +0530

    Validate if Title and Description is same

--- BandAPI/Models/AlbumForCreatingDto.cs
+++ BandAPI/Models/AlbumForCreatingDto.cs
@@ -1,15 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BandAPI.Models
 {
-    public class AlbumForCreatingDto
+    public class AlbumForCreatingDto : IValidatableObject
     {
+        [Required]
+        [MaxLength(100)]
         public string Title { get; set; }
 
+        [MaxLength(200)]
         public string Description { get; set; }
 
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(Title == Description)
+            { 
+                yield return new ValidationResult("Title and Description cannot be same", new[] { nameof(AlbumForCreatingDto) });
+            }
+        }
     }
 }
